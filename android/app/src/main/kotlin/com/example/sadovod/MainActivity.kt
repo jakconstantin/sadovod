d5af61b3b1f5971e572e2584eba75858e098f0c2
jakconstantin <jakconstantin@gmail.com>
2025-04-08 22:58:39 +0300

    Deploy web to GitHub Pages

--- android/app/src/main/kotlin/com/example/sadovod/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.sadovod
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/models/income_info.dart
+++
@@ -1,9 +0,0 @@
-class IncomeInfo {
-  late double _priceAll = 0;
-  double get priceAll =>_priceAll;
-  void set priceAll(double value) {
-    if (value != null) {
-      _priceAll = value;
-    }
-  }
-}
