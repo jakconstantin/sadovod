3a672822d7faa795a5031812468490c50dc11fa4
jakconstantin <jakconstantin@gmail.com>
2025-04-13 22:10:40 +0300

    Новое добавление нужной информации

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1564691053"
+    serviceWorkerVersion: "4165491634"
   }
 });
