e454b7f0f1930f21f2058478993a809caf484de8
jakconstantin <jakconstantin@gmail.com>
2025-05-07 23:48:38 +0300

    Revert "Новая версия с измененным User"
    This reverts commit 0767dcb4d59284f030df30638e37fc443c3004ed.

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -27,14 +27,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "35c02ddff0f87fc282ff6c89a86b14c7
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "247fc864a065a6a996d008e8ee2ddc84",
+"flutter_bootstrap.js": "26698d3d29646be9265334cefcbbe9bb",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "243026c3e04ac43c59b46487d3b5fc0f",
-"/": "243026c3e04ac43c59b46487d3b5fc0f",
-"main.dart.js": "1b6cb1b22e43ffd554a3002603c8cfd9",
+"index.html": "9a112a4e6f68322fdd37f5acad425002",
+"/": "9a112a4e6f68322fdd37f5acad425002",
+"main.dart.js": "b5cd7f6e99feb65ac4ad9a8f832458f5",
 "manifest.json": "d6291c06053b63c38e4ecfba234ea347",
 "version.json": "1bd90e00b65eaa136eba639ab59637ad"};
 // The application shell files that are downloaded before a service worker can
